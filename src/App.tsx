d2646ed9a85788f72ee92a1d4056a17af75c3de7
yrgk <yarik090908@gmail.com>
2025-03-26 01:49:43 +0300

    made up architecture; created watching ticket screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
+import WebApp from '@twa-dev/sdk'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react';
 
 import './App.css'
 import Ticket from './components/Ticket/Ticket'
 
 function App() {
+    const tg = WebApp
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
     return (
         <BrowserRouter>
         <Routes>
             {/* <Route index element={<h1>Main page</h1>}/> */}
 
             {/* Watching ticket */}
-            <Route index element={Ticket}/>
+            <Route index element={<Ticket/>}/>
+            {/* <Route path='ticket/:id' element={<Ticket/>}/> */}
 
             {/* Checking ticket */}
-            <Route index element={Ticket}/>
+            <Route path='ticket/check' element={<Ticket/>}/>
 
             {/* Watching event page */}
-            <Route index element={Ticket}/>
+            <Route path='event/:id' element={<Ticket/>}/>
 
             {/* Signing up to event */}
-            <Route index element={Ticket}/>
+            <Route path='event/sign' element={<Ticket/>}/>
 
             {/* Succesful page */}
-            <Route index element={Ticket}/>
+            <Route path='success' element={<Ticket/>}/>
 
         </Routes>
         </BrowserRouter>
--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,8 +1,32 @@
+import WebApp from '@twa-dev/sdk'
+import './Ticket.css'
+import { useEffect } from 'react';
+
 function Ticket() {
+    const tg = WebApp;
+
+    tg.MainButton.show();
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', function() {
+            tg.HapticFeedback.impactOccurred('light')
+            tg.close()
+        })
+    }, []);
+
+    tg.MainButton.setParams({
+        text: `Закрыть`
+    });
+
     return (
-        <>
-        
-        </>
+        <div className="qr-code-screen">
+            <div className="qr-code-block">
+                <img id="qr-code" src="https://storage.yandexcloud.net/ticket-bucket/123456789122024-12-17%2019%3A21%3A00.493884%20%2B0300%20MSK%20m%3D%2B60.649606917" alt="" />
+            </div>
+
+            <h3>Gorillaz "Humanz Tour" - Concert in Berlin</h3>
+            <h2>Standard</h2>
+        </div>
     )
 }
 
