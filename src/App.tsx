f42b8d554dc63771a0328851a086542e863866cc
yrgk <yarik090908@gmail.com>
2025-04-23 07:29:25 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Event from './components/Event/Event';
 import Form from './components/Form/Form';
 import Successful from './components/Successful/Successful';
 import Ticket from './components/Ticket/Ticket';
+import MainPage from './components/MainPage/MainPage';
 
 function App() {
     const tg = WebApp
@@ -21,29 +22,24 @@ function App() {
     return (
         <BrowserRouter>
         <Routes>
-            <Route index element={<Event/>}/>
+            {/* Main page */}
+            <Route index element={<MainPage/>}/>
+
+            {/* My projects page */}
+            <Route path='projects/my' element={<MainPage/>}/>
 
             {/* Watching ticket */}
             <Route path='ticket/:id' element={<Ticket/>}/>
 
-            {/* Camera */}
-            <Route path='camera' element={<Camera/>}/>
-
             {/* Checking ticket */}
             <Route path='ticket/check' element={<CheckTicket/>}/>
 
-            {/* Watching event page */}
-            <Route path='event/:id' element={<Event/>}/>
-
             {/* Make form */}
-            <Route path='form/make' element={<CreateForm/>}/>
+            <Route path='form/create' element={<CreateForm/>}/>
 
             {/* Watching form */}
             {/* <Route path='form/:id' element={<Form/>}/> */}
             <Route index element={<Form/>}/>
-
-            {/* Choose a book */}
-            <Route path='schedule/:id' element={<Successful/>}/>
 
             {/* Succesful page */}
             <Route path='success' element={<Successful/>}/>
